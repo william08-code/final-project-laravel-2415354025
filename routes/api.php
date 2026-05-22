97dcdc424cafc2d30c9c0b7d98dcfcef48e9d65a
william08-code <williamyehezkiel495@gmail.com>
2026-05-22 16:29:44 +0800

    adding api routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,18 @@
 <?php
+use App\Http\Controllers\Api\CustomerController;
 use App\Http\Controllers\Api\ServiceController;
+use App\Http\Controllers\Api\SubscriptionController;
 
-Route::apiResource('services', ServiceController::class);
+Route::get('customers', [CustomerController::class, 'index']);
+Route::get('customers/{id}', [CustomerController::class, 'show']);
+Route::post('customers', [CustomerController::class, 'store']);
+Route::put('customers/{id}', [CustomerController::class, 'update']);
+Route::delete('customers/{id}', [CustomerController::class, 'destroy']);
 
-Route::patch('services/{service}/activate', [ServiceController::class, 'activate']);
-Route::patch('services/{service}/deactivate', [ServiceController::class, 'deactivate']);
+Route::apiResource('subscriptions', SubscriptionController::class);
+
+Route::get('subscriptions/status/{status}', [SubscriptionController::class, 'getByStatus']);
+Route::patch('subscriptions/{id}/status', [SubscriptionController::class, 'changeStatus']);
+
+Route::get('customers/status/{status}', [CustomerController::class, 'getByStatus']);
+Route::patch('customers/{id}/status', [CustomerController::class, 'changeStatus']);
